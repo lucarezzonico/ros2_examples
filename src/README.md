aaf2dbf91d5d12c532887bba9e4eba348c7fd184
Luca Rezzonico <lucarezzonico97@gmail.com>
2023-10-05 18:45:24 +0200

    launch action server client

--- src/README.md
+++ src/README.md
@@ -1,2 +1,5 @@
 ## All released ROS2 Humble packages:
 https://github.com/ros/rosdistro/blob/master/humble/distribution.yaml
+
+## Some Demos
+https://github.com/ros2/demos/tree/humble
---
+++ src/action_tutorials_py/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'action_tutorials_py'
+
+setup(
+    name=package_name,
+    version='0.20.3',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Jacob Perron',
+    author_email='jacob@openrobotics.org',
+    maintainer='Audrow Nash, Michael Jeronimo',
+    maintainer_email='audrow@openrobotics.org, michael.jeronimo@openrobotics.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Python action tutorials code.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fibonacci_action_server = action_tutorials_py.fibonacci_action_server:main',
+            'fibonacci_action_client = action_tutorials_py.fibonacci_action_client:main',
+        ],
+    },
+)
